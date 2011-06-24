21d7382619a8e5059ad12dd00546da5936814238
"Joseph Lansdowne ext:(%22) <J49137@gmail.com>
2011-06-24 15:44:24 +0100

    update file list

---
+++ jsession.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+
+void logout (int sig) {
+    exit(EXIT_SUCCESS);
+}
+
+int main () {
+    signal(SIGTERM, logout);
+    getchar();
+    exit(EXIT_SUCCESS);
+}
